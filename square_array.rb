cd9f1762ecd46f80802e0a50fc0691dfc9c3ca9f
Stephen Junge <steve.junge@gmail.com>
2020-08-04 21:42:11 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,21 +1,10 @@
-require "pry"
+
 def square_array(numbers)
   new_numbers = []
   
   numbers.each do |number|
-    #binding.pry
     new_numbers.push(number ** 2 ) 
   end
   new_numbers
 end
 
-#square_array([1,2,3])
-#square_array([9,10,16,25])
-
-def square_array(numbers)
-  
-  numbers.collect do |number|
-    #binding.pry
-    number ** 2
-  end
-end
